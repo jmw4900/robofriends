2dbd809e5d9f7360fb9103c7c299742f9db8dc81
Minwoo <jmw4900@gmail.com>
2019-11-29 19:52:30 -0700

    Updates

---
+++ precache-manifest.3d5c92dbd913be0d201968394fcdf60c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a82924205348b7f804ff761d99e40d6",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "26dafc4245b14a37f593",
+    "url": "/robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "3d3fbc138b1b5b487de7",
+    "url": "/robofriends/static/css/main.d75e7015.chunk.css"
+  },
+  {
+    "revision": "26dafc4245b14a37f593",
+    "url": "/robofriends/static/js/2.1f39b60e.chunk.js"
+  },
+  {
+    "revision": "3d3fbc138b1b5b487de7",
+    "url": "/robofriends/static/js/main.9617bd6c.chunk.js"
+  },
+  {
+    "revision": "7b4f849e958ac4107787",
+    "url": "/robofriends/static/js/runtime-main.a1071882.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
